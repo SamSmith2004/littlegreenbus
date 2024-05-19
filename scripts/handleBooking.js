e9d67cf0c8f492262b4d17f5b27ac83623dd0358
SamSmith2004 <160005857+SamSmith2004@users.noreply.github.com>
2024-05-19 15:51:14 +0100

    added skeleton of passenger details

--- scripts/handleBooking.js
+++ scripts/handleBooking.js
@@ -262,15 +262,64 @@ function TicketReturnTime() {
     }
 }
 
+function CheckStudent() {
+    let bookingDetails = JSON.parse(sessionStorage.getItem('bookingDetails'));
+    if (bookingDetails.studentcount == 0) {
+        return;
+    }
+    let form = document.getElementById('passengerDetailsForm');
+    let inputdiv = document.createElement('div');
+    inputdiv.className = 'input-group';
+    let label = document.createElement('label');
+    label.for = 'studentID';
+    label.textContent = 'Student ID';
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'studentid';
+    input.name = 'studentid';
+    input.placeholder = 'Enter your student ID';
+    
+    inputdiv.appendChild(label);
+    inputdiv.appendChild(input);
+    form.appendChild(inputdiv);
+}
+
 function TimeSubmit() {
     let bookingDetails = JSON.parse(sessionStorage.getItem('bookingDetails'));
-    document.getElementById('submitTicketTime').addEventListener('click', event => {
+    let submitButton = document.getElementById('submitTicketTime');
+    submitButton.addEventListener('click', function (event) {
         event.preventDefault();
-
-        if (bookingDetails.ticketType === 'Single') {
-            if (bookkingDetails.time == ! '') {
-                window.location.href = '/booking-details.html';
-            }
+        if (hasSelectedTime === false) {
+            alert('Please select a time for your journey');
+        } else if (bookingDetails.ticketType === 'Return' && hasSelectedReturnTime === false) {
+            alert('Please select a return time for your journey');
+        } else {
+            window.location.href = '/booking-details.html';
         }
     });
+}
+function PassengerDetailsSubmit() {
+    if (document.getElementById('email').value.includes('@') === false) {
+        alert('Please enter a valid email address');
+    }
+    if (document.getElementById('email').value !== document.getElementById('confirmEmail').value) {
+        alert('Emails do not match');
+    }
+    if (document.getElementById('phone').value.length < 10 || document.getElementById('phone').value.length > 15 || isNaN(document.getElementById('phone').value) === true) {
+        alert('Please enter a valid phone number');
+    }
+
+    let passengerInfo = {
+        firstname: document.getElementById('firstname').value,
+        lastname: document.getElementById('lastname').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        address: document.getElementById('address').value,
+        city: document.getElementById('city').value,
+        postcode: document.getElementById('postcode').value
+    };
+    if (document.getElementById('studentid')) {
+        passengerInfo.studentid = document.getElementById('studentid').value;
+    }
+    console.log(passengerInfo);
 }
